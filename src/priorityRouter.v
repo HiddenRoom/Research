e7ff0b8cc72b8966408896d7d1cac57032aff467
Gamers Gaming <bestwitherskele@gmail.com>
2024-02-29 00:13:36 -0700

    added router description

---
+++ src/priorityRouter.v
@@ -0,0 +1,13 @@
+`ifndef __PRIORITYROUTER__
+`define __PRIORITYROUTER__
+
+module
+(
+  // take in input val and version num and read input with version num and
+  // output the nearest version inside with a version num less than input
+  // version num 
+);
+
+endmodule
+
+`endif
