24659ff810dd452a6d923cebad2f53630d855ec0
HiddenRoom <bestwitherskele@gmail.com>
2024-03-01 14:39:46 -0700

    register file template

---
+++ src/regFile.v
@@ -0,0 +1,15 @@
+`ifndef __REGFILE__
+`define __REGFILE__
+
+module regFile
+(
+  input [4:0]     read0,
+  input [4:0]     read1,
+  input [4:0]     read2,
+  input [4:0]     read3,
+);
+
+
+module
+
+`endif
