4a7a706d3fd3368f6da028ec81e1ffe313dc04e7
Gamers Gaming <bestwitherskele@gmail.com>
2024-03-22 22:21:52 -0600

    adjust multidimensonal port declarations as theyre not valid syntax

--- src/regFile.v
+++ src/regFile.v
@@ -3,41 +3,39 @@
 
 module regFile 
 #(
-    parameter REG_NUM = 32,
-    parameter DATA_WIDTH = 32,
-    parameter NUM_READ_PORTS = 4,
-    parameter NUM_WRITE_PORTS = 4
+  parameter REG_NUM = 32,
+  parameter DATA_WIDTH = 32,
+  parameter READ_NUM = 4,
+  parameter WRITE_NUM = 4
 ) 
 (
-    input                       clk,
-    input [$clog2(REG_NUM)-1:0] readAddr [0:NUM_READ_PORTS-1],
-    input [$clog2(REG_NUM)-1:0] writeAddr [0:NUM_WRITE_PORTS-1],
+  input                       clk,
+  input [$clog2(REG_NUM) * READ_NUM - 1:0] readAddr, 
+  input [$clog2(REG_NUM) * WRITE_NUM - 1:0] writeAddr, 
 
-    input                       writeEnable [0:NUM_WRITE_PORTS-1],
+  input [WRITE_NUM - 1:0] writeEnable, 
 
-    input  [DATA_WIDTH-1:0]     dataInputs [0:NUM_WRITE_PORTS-1],
+  input  [DATA_WIDTH * WRITE_NUM - 1:0] dataInputs, 
 
-    output reg [DATA_WIDTH-1:0] dataOuts [0:NUM_READ_PORTS-1]
+  output reg [DATA_WIDTH * READ_NUM - 1:0] dataOuts
 );
 
-    reg [DATA_WIDTH-1:0] regFile [0:REG_NUM-1];
+  reg [DATA_WIDTH - 1:0] regFile [0:REG_NUM - 1];
 
-    assign regFile[0] = 0;
-
-    always @(posedge clk) begin
-    end
+  assign regFile[0] = 0;
 
   always @(posedge clk) 
   begin
-    for (int i = 0; i < NUM_READ_PORTS; i++) begin
-      dataOuts[i] <= regFile[readAddr[i]];
+    for (int i = 0; i < READ_NUM; i++) 
+    begin
+      dataOuts[DATA_WIDTH * i +:DATA_WIDTH] <= regFile[readAddr[$clog2(REG_NUM) * i +:$clog2(REG_NUM)]];
     end
 
-    for (int i = 0; i < NUM_WRITE_PORTS; i++) 
+    for (int i = 0; i < WRITE_NUM; i++) 
     begin
       if (writeEnable[i]) 
       begin
-        regFile[writeAddr[i]] <= dataInputs[i];
+        regFile[writeAddr[$clog2(REG_NUM) *  i +:$clog2(REG_NUM)]] <= dataInputs[i];
       end
     end
   end
